81a89399b9fabff0d31a2fe11c2416ff6e47bb68
Shreya14150 <shreya.vasagadekar@cumminscollege.in>
2026-06-07 14:46:00 +0530

    Update app.py

--- Sales_Dashboard/app.py
+++ Sales_Dashboard/app.py
@@ -1,10 +1,8 @@
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
-
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
-
 # ---- READ EXCEL ----
 @st.cache_data
 def get_data_from_excel():
@@ -19,9 +17,7 @@ def get_data_from_excel():
     # Add 'hour' column to dataframe
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
     return df
-
 df = get_data_from_excel()
-
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
 city = st.sidebar.multiselect(
@@ -29,38 +25,31 @@ city = st.sidebar.multiselect(
     options=df["City"].unique(),
     default=df["City"].unique()
 )
-
 customer_type = st.sidebar.multiselect(
     "Select the Customer Type:",
     options=df["Customer_type"].unique(),
     default=df["Customer_type"].unique(),
 )
-
 gender = st.sidebar.multiselect(
     "Select the Gender:",
     options=df["Gender"].unique(),
     default=df["Gender"].unique()
 )
-
 df_selection = df.query(
     "City == @city & Customer_type ==@customer_type & Gender == @gender"
 )
-
 # Check if the dataframe is empty:
 if df_selection.empty:
     st.warning("No data available based on the current filter settings!")
     st.stop() # This will halt the app from further execution.
-
 # ---- MAINPAGE ----
 st.title(":bar_chart: Sales Dashboard")
 st.markdown("##")
-
 # TOP KPI's
 total_sales = int(df_selection["Total"].sum())
 average_rating = round(df_selection["Rating"].mean(), 1)
 star_rating = ":star:" * int(round(average_rating, 0))
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     st.subheader("Total Sales:")
@@ -71,9 +60,7 @@ with middle_column:
 with right_column:
     st.subheader("Average Sales Per Transaction:")
     st.subheader(f"INR rupees {average_sale_by_transaction}")
-
 st.markdown("""---""")
-
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = df_selection.groupby(by=["Product line"])[["Total"]].sum().sort_values(by="Total")
 fig_product_sales = px.bar(
@@ -89,7 +76,6 @@ fig_product_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
 )
-
 # SALES BY HOUR [BAR CHART]
 sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
 fig_hourly_sales = px.bar(
@@ -105,36 +91,15 @@ fig_hourly_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     yaxis=(dict(showgrid=False)),
 )
-
-
 left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 right_column.plotly_chart(fig_product_sales, use_container_width=True)
-
-
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    
-    .stApp {
-        background-color: #0E1117;
-        color: white;
-    }
-    [data-testid="stSidebar"] {
-        background-color: #1B6CA8;
-    }
-    [data-testid="stSidebar"] * {
-        color: white !important;
-    }
-    </style>
-"""
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
-
-
-
-
-
